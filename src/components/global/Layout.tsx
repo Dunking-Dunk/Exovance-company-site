aaba50cdf72d73c7feb9524fa70e81767724b420
Dunking-Dunk <hursunss@gmail.com>
2025-03-30 12:31:48 +0530

    added changes

--- src/components/global/Layout.tsx
+++ src/components/global/Layout.tsx
@@ -95,18 +95,7 @@ const Layout = ({ children }: Props) => {
                 <AnimatedCursor />
                 {children}
 
-                <Scene
-                    style={{
-                        position: 'fixed',
-                        top: 0,
-                        left: 0,
-                        width: '100vw',
-                        height: '100vh',
-                        pointerEvents: 'none',
-                    }}
-                    eventSource={ref}
-                    eventPrefix='client'
-                />
+                <Scene />
                 <LoadingScreen />
             </div>
         </ReactLenis>
